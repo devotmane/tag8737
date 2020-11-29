ddf373f2a9ebf33ce6c6c804bfc8a91115b2a0a6
otmane omry <omry.otmane@gmail.com>
2020-11-29 08:02:47 +0100

    Commit In Sun Nov 29 08:02:47 +01 2020

--- tag8737.js
+++ tag8737.js
@@ -1,1 +1,16 @@
-console.log('Hello ScriptTag')
+jQuery(document).ready(function() {	
+    $('body').prepend('<div class="header" id="myHeader"><h1> teste </h1></div>');
+    $('head').prepend('<style>.header { padding: 10px 16px; background: #555; color: #f1f1f1; } .content { padding: 16px; } .sticky { position: fixed; top: 0; width: 100%} .sticky + .content { padding-top: 102px; }</style>');
+
+    var header = document.getElementById("myHeader");
+    var sticky = header.offsetTop;
+
+    window.onscroll = function() { 
+        if (window.pageYOffset > sticky) {
+            header.classList.add("sticky");
+        } else {
+            header.classList.remove("sticky");
+        }
+    };
+
+});
