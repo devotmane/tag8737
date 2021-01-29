871c3401e643b399b1884612ff3fe078bab60c6d
otmane omry <omry.otmane@gmail.com>
2021-01-29 23:06:13 +0100

    #add files shopdulux

--- shopdulux.js
+++ shopdulux.js
@@ -1,1 +1,26 @@
-var input,el,inputs=document.querySelectorAll(".form-group input, .single-submit");function shake(){for(let s=0;s<inputs.length;s++)(input=inputs[s]).classList.contains("shake")?input.classList.remove("shake"):input.classList.contains("stopshake")||input.classList.add("shake")}for(let s=0;s<inputs.length;s++)inputs[s].addEventListener("focus",s=>{(el=s.explicitOriginalTarget).classList.contains("stopshake")&&!s.target.value||el.classList.add("stopshake")});setInterval("shake()",1e3);
+var inputs = document.querySelectorAll(".form-group input, .single-submit"), input, el;
+
+function shake() {
+    for (let i = 0; i < inputs.length; i++) {
+        input = inputs[i];
+        console.log(input.value);
+        if (input.classList.contains('shake')) {
+            input.classList.remove("shake");
+        } else {
+            if (!input.classList.contains('stopshake')) {
+                input.classList.add("shake");
+            }
+        }
+    }
+}
+
+for (let i = 0; i < inputs.length; i++) {
+        inputs[i].addEventListener('focus', (event) => {
+        el = event.explicitOriginalTarget;
+        if (!el.classList.contains('stopshake')) {
+            el.classList.add("stopshake");
+        }
+    });
+}
+
+    setInterval("shake()", 1000);
